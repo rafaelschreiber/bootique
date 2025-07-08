a0c6b71a72da0faaf321621bf79178623b4d1df7
Rafael Schreiber <rafael@backend.works>
2025-07-08 14:03:51 +0200

    Patches rocky repo

--- src/globals.py
+++ src/globals.py
@@ -9,7 +9,7 @@ import ConfigManager
 
 # configured/supported RHEL distributions and their repos
 DISTRIBUTION_REPOS = {
-    "rocky": "http://repo{version}.parlament.gv.at/{version}/BaseOS/{architecture}/os",
+    "rocky": "http://repo{version}.parlament.gv.at/{version}/BaseOS/{architecture}/kickstart",
     "rhel": "http://repo{version}.parlament.gv.at/RHEL/{version}/BaseOS"
 }
 
